04ac8a142d971bbb42ecb1f0d3093cb4f0af8686
Cameron Asbury <CameronAsbury@openarc.net>
2019-12-03 09:33:49 -0500

    dirty solution, but final code gets answer for part 2 of day 1, with some minor mods it would get the answer for part 1

--- AdventOfCodeDay1/Program.cs
+++ AdventOfCodeDay1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AdventOfCodeDay1
 {
@@ -6,7 +7,50 @@ namespace AdventOfCodeDay1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program P = new Program();
+            string line;
+            int counter = 0;
+            int totalFuel = 0;
+            int fuelFuel;
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                //String line = sr.ReadToEnd();
+                //Console.WriteLine(line);
+            }
+            StreamReader file = new StreamReader("input.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                counter++;
+                Console.WriteLine("Total Fuel So Far: " + (totalFuel += P.calculateFuel(Convert.ToInt32(line), counter)));
+            }
+
+            //fuelFuel = P.calculateFuel(totalFuel);
+
+            //totalFuel += P.calculateFuelForFuel(fuelFuel);
+            Console.WriteLine("Total Fuel Required: " + totalFuel);
+
+        }
+
+        public int calculateFuel(int moduleMass, int counter)
+        {
+            Program P = new Program();
+            int fuelReq;
+            fuelReq = (moduleMass / 3) - 2;
+            fuelReq += P.calculateFuelForFuel(fuelReq);
+            return fuelReq;
+        }
+
+        public int calculateFuelForFuel(int fuel)
+        {
+            int fuelReq;
+            fuelReq = (fuel / 3) - 2;
+            Console.WriteLine("Fuel Requirement in Recursive Function: " + fuelReq);
+            if (fuelReq <= 0)
+            {
+                return 0;
+            }
+            return fuelReq+calculateFuelForFuel(fuelReq);
         }
     }
 }
+                
